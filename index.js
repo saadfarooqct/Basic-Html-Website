b94d95c97a956053bca122479bb942030a88ac70
KingSaad <saadfarooqct@gmail.com>
2024-01-29 17:24:41 -0500

    .

---
+++ index.js
@@ -0,0 +1,11 @@
+const my_icon = document.getElementsByClassName("my-icon");
+
+my_icon.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "black";
+    event.target.textContent = "saadfarooqct@gmail.com";
+});
+
+my_icon.addEventListener("mouseout", event => {
+    event.target.style.backgroundColor = "black";
+    event.target.textContent = "saadfarooqct@gmail.com";
+});
