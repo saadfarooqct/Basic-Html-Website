242b5d2447ef71e1782aabe0b0e475599d06e522
KingSaad <saadfarooqct@gmail.com>
2024-01-29 19:34:42 -0500

    .

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const my_icon = document.getElementsByClassName("my-icon");
+const my_icon = document.getElementById("my-icon");
+const iconElement = my_icon.querySelector("ion-icon");
 
 my_icon.addEventListener("mouseover", event => {
-    event.target.style.backgroundColor = "black";
-    event.target.textContent = "saadfarooqct@gmail.com";
+    my_icon.style.backgroundColor = "black";
+    iconElement.setAttribute = "saadfarooqct@gmail.com";
 });
 
 my_icon.addEventListener("mouseout", event => {
-    event.target.style.backgroundColor = "black";
-    event.target.textContent = "saadfarooqct@gmail.com";
+    my_icon.style.backgroundColor = "";
+    iconElement.setAttribute = "";
 });
